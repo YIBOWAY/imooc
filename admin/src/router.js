f123768bfd1bd3a14f86f0103ce7d862e8e459b3
YIBOWAY <2338129627@qq.com>
2020-05-14 22:00:52 +0800

    1.增加login页面 2.增加路由配置，访问login页面时，加载login组件

---
+++ admin/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './views/login.vue'
+
+Vue.use(Router)
+
+
+export default new Router({
+    mode:'history',//还有一种hash格式，带有#号
+    base:process.env.BASE_URL,
+    routes:[{
+        path:'*',//无论输入什么，都会跳转到/login地址
+        redirect:"/login",
+    },{
+        path: '/login',
+        component:Login
+    }]
+})
