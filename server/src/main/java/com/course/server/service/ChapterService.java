0ea74f986a2872a86b9e12fc9bc106dcb61cf937
YIBOWAY <2338129627@qq.com>
2020-05-16 22:17:17 +0800

    1.增加dto层，用于controller和service层 2.增加chapterDto 3.修改ChapterService，将返回Chapter改成返回ChapterDto

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -2,20 +2,32 @@ package com.course.server.service;
 
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
+import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
-    public List<Chapter> list(){
+    public List<ChapterDto> list(){
         ChapterExample chapterExample = new ChapterExample();
-        chapterExample.createCriteria().andIdEqualTo("1");//sql中的where语句
-        chapterExample.setOrderByClause("id desc");//sql中的order by 语句
-        return chapterMapper.selectByExample(chapterExample);//select语句
+//        chapterExample.createCriteria().andIdEqualTo("1");//sql中的where语句
+//        chapterExample.setOrderByClause("id desc");//sql中的order by 语句
+        List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);//select语句
+        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
+        for (int i = 0, l = chapterList.size();i<l; i++) {
+            Chapter chapter = chapterList.get(i);
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter,chapterDto);//拷贝，把chapter中的值拷贝到数据传输层
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
     }
 }
