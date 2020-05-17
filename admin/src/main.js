d54d8c6fde049e1c76113a26ea407771825a59e4
YIBOWAY <2338129627@qq.com>
2020-05-17 22:18:13 +0800

    1.集成axios完成前后端交互 2.增加CorsConfig，解决前后端跨域问题

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false;
+Vue.prototype.$ajax = axios;//内置的属性，以后使用时调用这个属性
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
